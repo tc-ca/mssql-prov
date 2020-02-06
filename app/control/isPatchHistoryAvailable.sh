793e47a241db509bae31bee3d485b46b11dd6a46
TeamDman <TeamDman9201@gmail.com>
2020-02-06 14:01:42 -0500

    Fix "raiserror" having too many eeeeeeee

--- app/control/isPatchHistoryAvailable.sh
+++ app/control/isPatchHistoryAvailable.sh
@@ -1,13 +1,13 @@
 /opt/mssql-tools/bin/sqlcmd -l 30 -S localhost,1433 -h-1 -V1 -U sa -P $SA_PASSWORD -Q $'
 SET NOCOUNT ON
-IF DB_ID(\$(PATCH_HISTORY_DB)) IS NULL BEGIN
-	RAISEERROR (\'Patch history db doesn\'\'t exist!\', 1,1)
+IF DB_ID(\'$(PATCH_HISTORY_DB)\') IS NULL BEGIN
+	RAISERROR (\'Patch history db doesn\'\'t exist!\', 1,1)
 END
 
 USE [$(PATCH_HISTORY_DB)]
 GO
 
-IF OBJECT_ID($(PATCH_HISTORY_TABLE)) IS NULL BEGIN
+IF OBJECT_ID(\'$(PATCH_HISTORY_TABLE)\') IS NULL BEGIN
 	RAISERROR (\'Patch history table doesn\'\'t exist!\',1,1)
 END
-' > /dev/null
+' #> /dev/null
