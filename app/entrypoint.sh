056d235b6315b0ee7e04b71ca1268781654ad368
TeamDman <TeamDman9201@gmail.com>
2020-02-27 10:43:40 -0500

    Cleanup semicolons and . to source

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,15 +2,15 @@ source ./common.sh;
 awaitServer;
 checkPatchTable;
 if [ $? -ne 0 ] ; then
-	echo "Could not find patch history, performing first time setup provision."
-	. /app/provision.sh
+	echo "Could not find patch history, performing first time setup provision.";
+	source /app/provision.sh;
 	echo "Spoofing patch logs ...";
-	logOnly=1 # Provision step makes patching redundant, populate the table so the patches won't be run on future startups.
+	logOnly=1; # Provision step makes patching redundant, populate the table so the patches won't be run on future startups.
 fi
 
 checkPatchTable;
 if [ $? -ne 0 ] ; then
-	echo "Patch history table or db is missing. Please ensure the provision step creates the database and table properly."
+	echo "Patch history table or db is missing. Please ensure the provision step creates the database and table properly.";
 else 
-	. /app/patch.sh
+	source /app/patch.sh;
 fi
